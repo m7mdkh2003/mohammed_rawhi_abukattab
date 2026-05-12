eb0431ff13275000bcce339afb6e926ebf210c8b
TOP <momo2532364@gmail.com>
2026-05-12 13:17:12 +0300

    Add about page route and view

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    $name= 'm7md';
+    $departments=[
+        '1' =>'aas',
+        '2' => 'css',
+        '3' =>'dss'
+        ];
+  // return view('about',['name' => $name]);
+    //return view('about')->with('name' , $name);
+    return view('about' , compact('name','departments'));
+
+});
+
+Route::post('/about', function () {
+$name=$_POST['name'];
+$departments=[
+'1' =>'aas',
+'2' => 'css',
+'3' =>'dss'
+];
+return view('about', compact('name', 'departments'));
+});
