36a4f2531b005528313bf2295c66e034bcd0ab71
TOP <momo2532364@gmail.com>
2026-06-01 20:12:12 +0300

    Improved users and tasks UI

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -2,176 +2,266 @@
 
 @section('content')
 
-<div class="container mt-4">
-    <h1>User List App</h1>
+<div class="container-fluid">
 
-    <div class="offset-md-2 col-md-8">
+    <h1 class="h3 mb-4 text-gray-800 text-center">
+        Users Management System
+    </h1>
 
-        <div class="card">
+    <div class="text-center mb-4">
+        <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+             width="100">
+    </div>
 
-        @if (@isset($user))
+    <div class="row justify-content-center">
 
-            <div class="card-header">
-                Update User
+        <div class="col-lg-8">
+
+            @if(session('success'))
+
+            <div class="alert alert-success alert-dismissible fade show">
+
+                {{ session('success') }}
+
+                <button type="button"
+                        class="close"
+                        data-dismiss="alert">
+
+                    <span>&times;</span>
+
+                </button>
+
             </div>
 
-            <div class="card-body">
+            @endif
 
-                <form action="{{url('user/update')}}" method="POST">
+            <div class="card shadow mb-4">
 
-                    <input type="hidden" name="id" value="{{$user->id}}">
+                @if (@isset($user))
 
-                    @csrf
-
-                    <div class="mb-3">
-                        <label class="form-label">Name</label>
-
-                        <input type="text"
-                               name="name"
-                               class="form-control"
-                               value="{{$user->name}}">
-                    </div>
-
-                    <div class="mb-3">
-                        <label class="form-label">Email</label>
-
-                        <input type="email"
-                               name="email"
-                               class="form-control"
-                               value="{{$user->email}}">
-                    </div>
-
-                    <button type="submit" class="btn btn-primary">
+                <div class="card-header bg-info text-white">
+                    <h5 class="m-0">
+                        <i class="fas fa-edit"></i>
                         Update User
-                    </button>
+                    </h5>
+                </div>
 
-                </form>
+                <div class="card-body">
+
+                    <form action="{{url('user/update')}}" method="POST">
+
+                        <input type="hidden"
+                               name="id"
+                               value="{{$user->id}}">
+
+                        @csrf
+
+                        <div class="mb-3">
+
+                            <label class="form-label">
+                                Name
+                            </label>
+
+                            <input type="text"
+                                   name="name"
+                                   class="form-control"
+                                   value="{{$user->name}}">
+
+                        </div>
+
+                        <div class="mb-3">
+
+                            <label class="form-label">
+                                Email
+                            </label>
+
+                            <input type="email"
+                                   name="email"
+                                   class="form-control"
+                                   value="{{$user->email}}">
+
+                        </div>
+
+                        <button type="submit"
+                                class="btn btn-info">
+
+                            <i class="fas fa-save"></i>
+                            Update User
+
+                        </button>
+
+                    </form>
+
+                </div>
+
+                @else
+
+                <div class="card-header bg-primary text-white">
+                    <h5 class="m-0">
+                        <i class="fas fa-user-plus"></i>
+                        Add New User
+                    </h5>
+                </div>
+
+                <div class="card-body">
+
+                    <form action="{{url('user/create')}}" method="POST">
+
+                        @csrf
+
+                        <div class="mb-3">
+
+                            <label class="form-label">
+                                Name
+                            </label>
+
+                            <input type="text"
+                                   name="name"
+                                   class="form-control"
+                                   placeholder="Enter Name">
+
+                        </div>
+
+                        <div class="mb-3">
+
+                            <label class="form-label">
+                                Email
+                            </label>
+
+                            <input type="email"
+                                   name="email"
+                                   class="form-control"
+                                   placeholder="Enter Email">
+
+                        </div>
+
+                        <div class="mb-3">
+
+                            <label class="form-label">
+                                Password
+                            </label>
+
+                            <input type="password"
+                                   name="password"
+                                   class="form-control"
+                                   placeholder="Enter Password">
+
+                        </div>
+
+                        <button type="submit"
+                                class="btn btn-primary">
+
+                            <i class="fas fa-plus"></i>
+                            Add User
+
+                        </button>
+
+                    </form>
+
+                </div>
+
+                @endif
 
             </div>
 
-        @else
+            <div class="card shadow mb-4">
 
-            <div class="card-header">
-                New User
-            </div>
+                <div class="card-header bg-success text-white">
 
-            <div class="card-body">
+                    <h5 class="m-0">
 
-                <form action="{{url('user/create')}}" method="POST">
+                        <i class="fas fa-users"></i>
+                        Current Users
 
-                    @csrf
+                    </h5>
 
-                    <div class="mb-3">
-                        <label class="form-label">Name</label>
+                </div>
 
-                        <input type="text"
-                               name="name"
-                               class="form-control">
+                <div class="card-body">
+
+                    <div class="table-responsive">
+
+                        <table class="table table-bordered table-hover">
+
+                            <thead class="thead-dark">
+
+                                <tr>
+
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th width="220">Actions</th>
+
+                                </tr>
+
+                            </thead>
+
+                            <tbody>
+
+                            @foreach ($users as $user)
+
+                                <tr>
+
+                                    <td>{{$user->id}}</td>
+
+                                    <td>{{$user->name}}</td>
+
+                                    <td>{{$user->email}}</td>
+
+                                    <td>
+
+                                        <form action="/user/delete/{{$user->id}}"
+                                              method="POST"
+                                              class="d-inline">
+
+                                            @csrf
+
+                                            <button type="submit"
+                                                    class="btn btn-danger btn-sm">
+
+                                                <i class="fas fa-trash"></i>
+                                                Delete
+
+                                            </button>
+
+                                        </form>
+
+                                        <form action="/user/edit/{{$user->id}}"
+                                              method="POST"
+                                              class="d-inline">
+
+                                            @csrf
+
+                                            <button type="submit"
+                                                    class="btn btn-info btn-sm">
+
+                                                <i class="fas fa-edit"></i>
+                                                Edit
+
+                                            </button>
+
+                                        </form>
+
+                                    </td>
+
+                                </tr>
+
+                            @endforeach
+
+                            </tbody>
+
+                        </table>
+
                     </div>
 
-                    <div class="mb-3">
-                        <label class="form-label">Email</label>
-
-                        <input type="email"
-                               name="email"
-                               class="form-control">
-                    </div>
-
-                    <div class="mb-3">
-                        <label class="form-label">Password</label>
-
-                        <input type="password"
-                               name="password"
-                               class="form-control">
-                    </div>
-
-                    <button type="submit" class="btn btn-primary">
-                        Add User
-                    </button>
-
-                </form>
-
-            </div>
-
-        @endif
-
-        </div>
-
-        <div class="card mt-4">
-
-            <div class="card-header">
-                Current Users
-            </div>
-
-            <div class="card-body">
-
-                <table class="table table-striped">
-
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-
-                    @foreach ($users as $user)
-
-                        <tr>
-
-                            <td>{{$user->name}}</td>
-
-                            <td>{{$user->email}}</td>
-
-                            <td>
-
-                                <form action="/user/delete/{{$user->id}}"
-                                      method="POST"
-                                      class="d-inline">
-
-                                    @csrf
-
-                                    <button type="submit"
-                                            class="btn btn-danger">
-
-                                        Delete
-
-                                    </button>
-
-                                </form>
-
-                                <form action="/user/edit/{{$user->id}}"
-                                      method="POST"
-                                      class="d-inline">
-
-                                    @csrf
-
-                                    <button type="submit"
-                                            class="btn btn-info">
-
-                                        Edit
-
-                                    </button>
-
-                                </form>
-
-                            </td>
-
-                        </tr>
-
-                    @endforeach
-
-                    </tbody>
-
-                </table>
+                </div>
 
             </div>
 
         </div>
 
     </div>
+
 </div>
 
 @endsection
